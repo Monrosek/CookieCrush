0ee7fed2ab7eca04fc008df3199c73f7aa5a623c
Mac <mac@MacBook-Pro.local>
2017-11-25 19:22:54 -0500

    Initial Commit

---
+++ CookieCrush/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CookieCrush
+//
+//  Created by Mac on 11/25/17.
+//  Copyright © 2017 Mobile Apps Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
